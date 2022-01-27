812e8e7910376513a7fb0ecaf0eda653d4f2c145
klezi10 <74952593+klezi10@users.noreply.github.com>
2022-01-27 10:25:49 +0700

    set up separate function for calculating monthly payment

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const loanAmount = document.getElementById('principal-loan-amount');
+let loanAmount = document.getElementById('principal-loan-amount');
 const interestRate = document.getElementById('interest-rate');
 const loanLength = document.getElementById('length-of-loan');
 const monthlyPayment = document.getElementById('monthly-payment');
@@ -7,7 +7,13 @@ const form = document.getElementById('form');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  calculatePayment();
+  monthPayment();
 });
 
-function calculatePayment() {}
+function monthPayment() {
+  let i = interestRate.value / 100 / 12;
+  let p = loanAmount.value;
+  let n = loanLength.value * 12;
+  let pay = (p * i * Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1);
+  console.log(pay);
+}
