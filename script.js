6711a05019ba074e769c63fa6631cd757953c4f9
klezi10 <74952593+klezi10@users.noreply.github.com>
2022-01-27 10:36:12 +0700

    set up text to update in the DOM

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 let loanAmount = document.getElementById('principal-loan-amount');
 const interestRate = document.getElementById('interest-rate');
 const loanLength = document.getElementById('length-of-loan');
-const monthlyPayment = document.getElementById('monthly-payment');
+const monthlyPaymentText = document.getElementById('monthly-payment');
 const calculateBtn = document.getElementById('calculate-btn');
 const form = document.getElementById('form');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  monthPayment();
+  monthlyPayment();
 });
 
-function monthPayment() {
+function monthlyPayment() {
   let i = interestRate.value / 100 / 12;
   let p = loanAmount.value;
   let n = loanLength.value * 12;
-  let pay = (p * i * Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1);
-  console.log(pay);
+  const payment = Math.round(
+    (p * i * Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1)
+  );
+  monthlyPaymentText.innerHTML = `Your monthly mortgate payment will be $${payment}.`;
 }
